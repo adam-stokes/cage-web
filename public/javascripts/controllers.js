7ffefaad2664075920861b18c05895dfb42f4166
Adam Stokes <adam.stokes@ubuntu.com>
2013-11-14 12:10:56 -0500

    use activity
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -4,10 +4,10 @@ var cagewebControllers = angular.module('cagewebControllers', []);
 
 cagewebControllers.controller('IndexCtrl', ['$scope', '$http',
     function($scope, $http) {
-        $http.get('http://localhost:9000/entity/page').
+        $http.get('http://localhost:9000/entity/activity').
         success(function(data, status, headers, config) {
-            $scope.pages = data;
-            console.log($scope.pages);
+            $scope.activity = data;
+            console.log($scope.activity);
         });
     }
 ]);
