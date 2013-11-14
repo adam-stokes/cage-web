a70c89f49338d6541b6dfada3418544d049cc206
Adam Stokes <adam.stokes@ubuntu.com>
2013-11-13 20:00:35 -0500

    blah blah
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,15 @@
  * GET home page.
  */
 
+var request = require('request');
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    request('http://localhost:9000/entity/activity', function(err, resp, body) {
+	res.render('index', { title: 'CAGE-web actvitiy app.'
+			      , activityList: body });
+    });
+};
+
+exports.addActivity = function(req, res){
+    res.send('haiiiii');
 };
